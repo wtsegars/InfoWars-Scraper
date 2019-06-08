6563a2678e4d4f55001ad6cae4fe4046095163aa
Darkangel <wtsegars@mac.com>
2019-06-08 11:20:53 -0400

    Finished with the models

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const CommentSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: false,
+        default: "Anyomous"
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
