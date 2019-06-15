57c787c19e9d893e71ded22b8b32c464d0fe473d
Darkangel <wtsegars@mac.com>
2019-06-14 22:49:25 -0400

    Updated the front end js

--- public/js/app.js
+++ public/js/app.js
@@ -2,37 +2,32 @@ $(document).ready(function() {
     $.getJSON("/articles", function(data) {
         for (let i = 0; i < 10; i++) {
             console.log(data)
-            $('#articles').append("<p id=articleData data-id='" + data[i]._id + "'>" + data[i].title + "<br /> <img src='" + data[i].image + "'/> <br /> <div id= article-summary>" + data[i].summary + "</div> <br /> <div id=data-category>Category:" + data[i].category + "</div> <br /> <div id=dataurl><a href='" + data[i].url + "'>Link</a></div></p>")
+            $('#articles').append("<p id='articleData' data-id='" + data[i]._id + "'>" + data[i].title + "<br /> <img src='" + data[i].image + "'/> <br /> <div id= article-summary>" + data[i].summary + "</div> <br /> <div id=data-category>Category:" + data[i].category + "</div> <br /> <div id=dataurl><a href='" + data[i].url + "'>Link</a></div></p><h2>Comment:</h2><h4>Name:</h4><input id='userId' name='user' ><h4>Title:</h4><input id='commentTitle' name='title' ><h4>Comment:</h4><textarea id='bodyinput' name='commentbody'></textarea><button data-id='" + data._id + "' id='saveComment'>Submit</button>");
         }
     });
     
-    $("#renderButton").on("click", function() {
-        $("").empty();
+    // $("#saveComment").on("click", function() {
+    //     $("userId").empty();
+    //     $("commentTitle").empty();
+    //     $("bodyInput").empty();
     
-        const thisId = $(this).attr("#data-id");
+    //     const thisId = $(this).attr("#data-id");
     
-        $.ajax({
-            method: "GET",
-            url: "/articles/" + thisId
-        }).then(function(data) {
-            console.log(data);
-            $("#articleData").append("<h2>Comment:</h2>");
-            $("#articleData").append("<h4>Name:</h4>");
-            $("#articleData").append("<input id='userId' name='user' >");
-            $("#articleData").append("<h4>Title:</h4>");
-            $("#articleData").append("<input id='commentTitle' name='title' >");
-            $("#articleData").append("<h4>Comment:</h4>");
-            $("#articleData").append("<textarea id='bodyinput' name='commentbody'></textarea>");
-            $("#articleData").append("<button data-id='" + data._id + "' id='savecomment'></button>");
+    //     $.ajax({
+    //         method: "GET",
+    //         url: "/articles/" + thisId
+    //     }).then(function(data) {
+    //         console.log(data);
     
-            if (data.comment) {
-                $("#userId").val(data.comment.user);
-                $("#bodyinput").val(data.comment.body);
-            }
-        });
-    });
+    //         if (data.comment) {
+    //             $("#userId").val(data.comment.user);
+    //             $("#commentTitle").val(data.comment.title);
+    //             $("#bodyinput").val(data.comment.body);
+    //         }
+    //     });
+    // });
     
-    $(document).on("click", "#savecomment", function() {
+    $(document).on("click", "#saveComment", function() {
         const thisId = $(this).attr("data-id");
     
         $.ajax({
