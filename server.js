354a1fcc1e8730a1eeb24b7b603610428cb225dd
Darkangel <wtsegars@mac.com>
2019-07-10 17:21:46 -0400

    updated server

--- server.js
+++ server.js
@@ -110,6 +110,10 @@ app.post("/comment", function(req, res) {
     });
 });
 
-app.listen(PORT, function() {
-    console.log("App running on port " + PORT + "!");
-});
+// app.listen(PORT, function() {
+//     console.log("App running on port " + PORT + "!");
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
